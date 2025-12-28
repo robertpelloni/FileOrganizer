d7eb483965bd49be40e0cea303018c52952bd25c
Robert Pelloni <pelloni.robert@gmail.com>
2025-12-27 22:54:02 -0500

    Add directory selection and scan UI to main window
    Replaces the label with a layout containing a path input, browse button, scan button, and log area. Implements directory selection and scan initiation logic, preparing the UI for integration with the core engine.

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,12 +1,55 @@
 #include "mainwindow.h"
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("FileOrganizer");
     resize(800, 600);
-    
-    label = new QLabel("Hello from Qt + fo_core!", this);
-    setCentralWidget(label);
+
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+
+    // Top bar: Path input and Browse button
+    QHBoxLayout *topLayout = new QHBoxLayout();
+    pathInput = new QLineEdit(this);
+    pathInput->setPlaceholderText("Select directory to scan...");
+    browseBtn = new QPushButton("Browse", this);
+    connect(browseBtn, &QPushButton::clicked, this, &MainWindow::selectDirectory);
+
+    topLayout->addWidget(pathInput);
+    topLayout->addWidget(browseBtn);
+    mainLayout->addLayout(topLayout);
+
+    // Scan button
+    scanBtn = new QPushButton("Start Scan", this);
+    connect(scanBtn, &QPushButton::clicked, this, &MainWindow::startScan);
+    mainLayout->addWidget(scanBtn);
+
+    // Log area
+    logArea = new QTextEdit(this);
+    logArea->setReadOnly(true);
+    mainLayout->addWidget(logArea);
 }
 
 MainWindow::~MainWindow() {
+}
+
+void MainWindow::selectDirectory() {
+    QString dir = QFileDialog::getExistingDirectory(this, "Select Directory",
+                                                    QDir::homePath(),
+                                                    QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    if (!dir.isEmpty()) {
+        pathInput->setText(dir);
+    }
+}
+
+void MainWindow::startScan() {
+    QString dir = pathInput->text();
+    if (dir.isEmpty()) {
+        logArea->append("Please select a directory first.");
+        return;
+    }
+    logArea->append("Scanning: " + dir);
+    // TODO: Integrate with fo_core Engine
 }
--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -1,12 +1,26 @@
 #pragma once
 #include <QMainWindow>
 #include <QLabel>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFileDialog>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void selectDirectory();
+    void startScan();
+
 private:
-    QLabel *label;
+    QLineEdit *pathInput;
+    QPushButton *browseBtn;
+    QPushButton *scanBtn;
+    QTextEdit *logArea;
 };
